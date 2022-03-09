4c136d197565463e2781b5813ae9c5537b82a1dc
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-03-08 22:48:27 -0300

    Initial structure

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { FormEvent, useState } from 'react';
+import { SearchResults } from '../components/SearchResults';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState<
+    { id: number; price: number; title: string }[]
+  >([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const products = await response.json();
+    setResults(products);
+  }
+
+  return (
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      {results && <SearchResults results={results} />}
+    </div>
+  );
+};
+
+export default Home;
